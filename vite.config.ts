f9091264007e2082a608fe9589fc99d8f73f6c55
Левков Глеб <kiss-shot-heart-under-blade@yandex.by>
2023-11-23 18:21:27 +0300

    ci: inline assets build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
+import { viteSingleFile } from 'vite-plugin-singlefile';
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [
+		preact(),
+		viteSingleFile(),
+
+	],
 });
+
+//<script type="module" crossorigin src="/assets/index-3f277a05.js"></script>
+//<link rel="stylesheet" href="/assets/index-e7275a14.css">
