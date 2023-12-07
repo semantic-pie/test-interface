91eda7741900d53c099b30f39070e1870c8606c2
Левков Глеб <kiss-shot-heart-under-blade@yandex.by>
2023-12-07 14:13:34 +0300

    dev: default port -> 3000

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import preact from '@preact/preset-vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		port: 3000,
+	},
 	plugins: [
 		preact(),
 		viteSingleFile(),
