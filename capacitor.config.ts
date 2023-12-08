32b9f9235051a8cc23ba26d4ad20bb347250f25c
Vladimir Moshchuk <ardonplay@gmail.com>
2023-12-08 22:59:16 +0300

    feat: mobile

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.unnamed',
+  appName: 'unnamed',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
