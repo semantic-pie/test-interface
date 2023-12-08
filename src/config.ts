04d65124192bad7ed0c96213835a396531ed25f6
Левков Глеб <kiss-shot-heart-under-blade@yandex.by>
2023-12-08 22:22:33 +0300

    quick-fix: remove hardcode ip

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // import viteConfig from "../vite.config"
 
 // const host =  '0.0.0.0'
-const host =  '192.168.31.121'
+const host =  '0.0.0.0'
 export const TRACKS_PER_PAGE = 10
 export const DOWNLOAD_TRACK_URL = `http://${host}:8080/api/v1/loafloader` // id
 
